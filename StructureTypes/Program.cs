5518bdb05d6f7331a3bd626aa235f7bc54386850
Antoni <84175644+Atolio-cold@users.noreply.github.com>
2024-11-07 00:28:28 +0100

    Add files via upload

--- StructureTypes/Program.cs
+++ StructureTypes/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+
+public record Coords(double X, double Y);  // Używamy 'record' zamiast 'struct'
+
+class Program
+{
+    public static void Main()
+    {
+        var p1 = new Coords(0, 0);
+        Console.WriteLine(p1);  // output: Coords { X = 0, Y = 0 }
+
+        var p2 = p1 with { X = 3 };
+        Console.WriteLine(p2);  // output: Coords { X = 3, Y = 0 }
+
+        var p3 = p1 with { X = 1, Y = 4 };
+        Console.WriteLine(p3);  // output: Coords { X = 1, Y = 4 }
+    }
+}
+
+
+[System.Runtime.CompilerServices.InlineArray(10)]
+public struct CharBuffer
+{
+    private char _firstElement;
+}
